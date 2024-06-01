35b676609e7aa7fd4a240baf5b0142f676e3ae9e
Lucas Daniel <ludaniel.sd@gmail.com>
2024-05-31 23:47:52 -0300

    Adicionando elementos na lista e testando os metodos criados

--- src/List/OperadoresBasicos/Livros/CatalogoLivros.java
+++ src/List/OperadoresBasicos/Livros/CatalogoLivros.java
@@ -6,15 +6,21 @@ import java.util.List;
 public class CatalogoLivros {
 	private List<Livro> listLivro;
 	
-	@SuppressWarnings("unused")
-	private void construc() {
+	
+	
+	public CatalogoLivros() {
 		this.listLivro = new ArrayList<>();
+	}
+
+	//metodo para adicionar livros na lista
+	public void adicionarLivros(String titulo, String autor, int ano_publicacao) {
+		listLivro.add(new Livro(titulo, autor, ano_publicacao));
 	}
 	
 	public List<Livro> pesquisarPorAutor(String autor){
 		List<Livro> livrosPorAutor = new ArrayList<>();
-		if(!livrosPorAutor.isEmpty()) {
-			for (Livro livro : livrosPorAutor) {
+		if(!listLivro.isEmpty()) {
+			for (Livro livro : listLivro) {
 				if(livro.getAutor().equalsIgnoreCase(autor)) {
 					livrosPorAutor.add(livro);
 				}
@@ -25,8 +31,8 @@ public class CatalogoLivros {
 	
 	public List<Livro> pesquisarPorIntervalo(int anoInicial,int anoFinal){
 		List<Livro> livrosPorIntervalo = new ArrayList<>();
-		if(!livrosPorIntervalo.isEmpty()) {
-			for (Livro livro : livrosPorIntervalo) {
+		if(!listLivro.isEmpty()) {
+			for (Livro livro : listLivro) {
 				if(livro.getAno_publicacao()>=anoInicial && livro.getAno_publicacao()<=anoFinal) {
 					livrosPorIntervalo.add(livro);
 				}
@@ -36,7 +42,7 @@ public class CatalogoLivros {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<Livro> pesquisarPorTitulo(String titulo){
+	public Livro pesquisarPorTitulo(String titulo){
 		Livro livroPorTitulo = null;
 		if(!listLivro.isEmpty()){
 			for (Livro livro : listLivro) {
@@ -46,8 +52,23 @@ public class CatalogoLivros {
 				}
 			}
 		}
-		return (List<Livro>) livroPorTitulo;
+		return  livroPorTitulo;
 	}
 	
-	
+
+	public static void main(String[] args) {
+		CatalogoLivros catalagolivros = new CatalogoLivros();
+		catalagolivros.adicionarLivros("Livro 1", "Autor 1", 2020);
+		catalagolivros.adicionarLivros("Livro 1", "Autor 2", 2021);
+		catalagolivros.adicionarLivros("Livro 2", "Autor 2", 2022);
+		catalagolivros.adicionarLivros("Livro 3", "Autor 3", 2023);
+		catalagolivros.adicionarLivros("Livro 4", "Autor 4", 1994);
+		
+		System.out.println("Pesquisa por Titulo:");
+		System.out.println(catalagolivros.pesquisarPorTitulo("livro 2"));
+		System.out.println("Pesquisa por Autor:");
+		System.out.println(catalagolivros.pesquisarPorAutor("Autor 2"));
+		System.out.println("Pesquisa por Ano de lançamento:");
+		System.out.println(catalagolivros.pesquisarPorIntervalo(2020, 2022));
+	}
 }
