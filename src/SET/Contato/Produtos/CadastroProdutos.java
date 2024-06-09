6e9d058a810df3045cf065e2013feb45fce0c187
Lucas Daniel <ludaniel.sd@gmail.com>
2024-06-08 23:53:18 -0300

    Testando os metodos criados para exibir produtos por nome usando Set

--- src/SET/Contato/Produtos/CadastroProdutos.java
+++ src/SET/Contato/Produtos/CadastroProdutos.java
@@ -5,19 +5,37 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class CadastroProdutos {
-	private Set<Produto> produtoSet;
-	
-	public CadastroProdutos() {
-		this.produtoSet = new HashSet<>();
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(String nome, long codigo, double preco, int qtd) {
+        produtoSet.add(new Produto(nome, codigo, preco, qtd));
+    }
+
+    public Set<Produto> exibirProdutoPorNome() {
+        return new TreeSet<>(produtoSet);
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> ProdutosPorPreco = new TreeSet<>(new Produto.CompararPorPreco());
+        ProdutosPorPreco.addAll(produtoSet);
+        return ProdutosPorPreco;
+    }
+    
+    public static void main(String[] args) {
+		CadastroProdutos cadastroProdutos = new CadastroProdutos();
+		cadastroProdutos.adicionarProduto("Produto 2", 8L, 15, 2);
+		cadastroProdutos.adicionarProduto("Produto 7", 2L, 15, 5);
+		cadastroProdutos.adicionarProduto("Produto 4", 5L, 15, 3);
+		cadastroProdutos.adicionarProduto("Produto 3", 2L, 15, 2);
+		
+		System.out.println(cadastroProdutos.produtoSet);
+		
+		System.out.println(cadastroProdutos.exibirProdutoPorNome());
+		
+		System.out.println(cadastroProdutos.exibirProdutosPorPreco());
 	}
-	public void adicionarProduto(String nome,long codigo,double preco,int qtd) {
-		produtoSet.add(new Produto(nome, codigo, preco, qtd));
-	}
-	
-	public Set<Produto> exibirProdutoPorNome() {
-		Set<Produto> ProdutosPorNome = new TreeSet<>(produtoSet);
-		return ProdutosPorNome;
-	}
-	
-	
 }
--- src/SET/Contato/Produtos/Produto.java
+++ src/SET/Contato/Produtos/Produto.java
@@ -3,71 +3,85 @@ package SET.Contato.Produtos;
 import java.util.Comparator;
 import java.util.Objects;
 
-public class Produto implements Comparable<Produto>{
-	
-	private String nome;
-	private long codigoProduto;
-	private double preco;
-	private int quantidade;
-	
-	public Produto(String nome, long codigoProduto, double preco, int quantidade) {
-		this.nome = nome;
-		this.codigoProduto = codigoProduto;
-		this.preco = preco;
-		this.quantidade = quantidade;
-	}
-	
-	@Override
-	public int compareTo(Produto p) {
-		// TODO Auto-generated method stub
-		return nome.compareToIgnoreCase(p.getNome());
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(codigoProduto);
-	}
+public class Produto implements Comparable<Produto> {
+
+    private String nome;
+    private long codigoProduto;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, long codigoProduto, double preco, int quantidade) {
+        this.nome = nome;
+        this.codigoProduto = codigoProduto;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigoProduto);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        return codigoProduto == other.codigoProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(long codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public static class CompararPorPreco implements Comparator<Produto> {
+        @Override
+        public int compare(Produto p1, Produto p2) {
+            return Double.compare(p1.getPreco(), p2.getPreco());
+        }
+    }
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produto other = (Produto) obj;
-		return codigoProduto == other.codigoProduto;
+	public String toString() {
+		return nome + " , " + codigoProduto + " , " + preco + " , "
+				+ quantidade;
 	}
-
-
-
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public long getCodigoProduto() {
-		return codigoProduto;
-	}
-	public void setCodigoProduto(int codigoProduto) {
-		this.codigoProduto = codigoProduto;
-	}
-	public double getPreco() {
-		return preco;
-	}
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-	public int getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-	
+    
 }
-
-
-
