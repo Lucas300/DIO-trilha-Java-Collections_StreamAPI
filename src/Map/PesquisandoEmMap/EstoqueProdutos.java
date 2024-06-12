a1adcb3e11f2ab64df6d0711af3af035f7859cf3
Lucas Daniel <ludaniel.sd@gmail.com>
2024-06-11 22:54:13 -0300

    trabalhando com pesquisas usando MAP

---
+++ src/Map/PesquisandoEmMap/EstoqueProdutos.java
@@ -0,0 +1,40 @@
+package Map.PesquisandoEmMap;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class EstoqueProdutos {
+	private Map<Long, Produto> estoqueProdutosMap;
+	
+	public void adicionarProduto(long cod, String nome,int quantidade, double preco) {
+		estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+	}
+	
+	public void exibirProdutos() {
+		System.out.println(estoqueProdutosMap);
+	}
+	
+	public double calcularValorTotalEstoque() {
+		double valorTotalEstoque = 0d;
+		if (!estoqueProdutosMap.isEmpty()) {
+			for (Produto p:estoqueProdutosMap.values()) {
+				valorTotalEstoque += p.getQuantidade() * p.getPreco();
+			}
+		}
+		return valorTotalEstoque;
+	}
+	
+	public Produto obterProdutoMaisCaro() {
+		Produto produtoMaisCaro = null;
+		double maiorPreco = Double.MIN_VALUE;
+		if (!estoqueProdutosMap.isEmpty()) {
+			for (Produto p:estoqueProdutosMap.values()) {
+				if (p.getPreco() > maiorPreco) {
+					produtoMaisCaro = p;
+				}
+			}
+		}
+		return produtoMaisCaro;
+	}
+	
+}
