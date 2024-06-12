88d00a0b91d27de726062e140709137abb2584b2
Lucas Daniel <ludaniel.sd@gmail.com>
2024-06-12 08:43:27 -0300

    Ordenando Map

---
+++ src/Map/Ordenacao_Map/Evento.java
@@ -0,0 +1,32 @@
+package Map.Ordenacao_Map;
+
+import java.time.LocalDate;
+
+public class Evento {
+
+	private String nome; 
+	private String  atracao;
+
+	public Evento(String nome, String atracao) {
+		this.nome = nome;
+		this.atracao = atracao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getAtracao() {
+		return atracao;
+	}
+
+	@Override
+	public String toString() {
+		return "nome=" + nome + ", atracao=" + atracao ;
+	}
+	
+	
+	
+	
+}
+
