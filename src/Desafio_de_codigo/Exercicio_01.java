c2a24a9ca824641cd001c7a18644ca15fdcfbdbd
Lucas Daniel <ludaniel.sd@gmail.com>
2024-07-02 21:00:23 -0300

    Adicionando o primeiro exercicio do desafio de codigo - Nesse desafio era necessario mostrar um array das transações realizadas no programa

---
+++ src/Desafio_de_codigo/Exercicio_01.java
@@ -0,0 +1,50 @@
+package Desafio_de_codigo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//RegistroTransacoesBancarias
+public class Exercicio_01{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = scanner.nextDouble();
+        int quantidadeTransacoes = scanner.nextInt();
+
+        // Lista para armazenar as transações
+        List<String> transacoes = new ArrayList<>();
+
+        
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+            char tipoTransacao = scanner.next().toUpperCase().charAt(0);
+            
+            double valorTransacao = scanner.nextDouble();
+
+            // Atualiza o saldo da conta e adicionar a transação à lista
+            if (tipoTransacao == 'D') {
+                saldo += valorTransacao;
+                transacoes.add("Deposito de " + valorTransacao);
+            } else if (tipoTransacao == 'S') {
+                saldo -= valorTransacao;
+                transacoes.add("Saque de " + valorTransacao);
+            } else {
+                System.out.println("Opção inválida. Utilize D para depósito ou S para saque.");
+                i--; // Decrementa o índice para repetir a iteração
+            }
+        }
+
+        
+        // TODO: Exibir o saldo final e a lista de transações conforme a tabela de Exemplos.
+        System.out.println("Saldo: "+saldo);
+        System.out.println("Transacoes:");
+        int i = 1;
+        for(String tr: transacoes){
+          System.out.println(i+". "+tr);
+          i++;
+        }
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
